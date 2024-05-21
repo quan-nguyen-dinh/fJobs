f5a04ab607bd6aac0ca62e0e7d8a14677adc21a6
quan nguyen dinh <quantalaquan@gmail.com>
2024-05-21 22:39:58 +0700

    implement ui zero kit

--- app/(tabs)/chat/index.js
+++ app/(tabs)/chat/index.js
@@ -7,6 +7,7 @@ import {REACT_APP_DEV_MODE} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import jwt_decode from "jwt-decode";
 import MessageItem from '../../../components/MessageItem';
+import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn'
 
 function index() {
   const [friends, setFriends] = useState([]);
@@ -34,51 +35,51 @@ function index() {
     router.push(`/message/${messageId}`);
   };
   return (
-    <View style={styles.container}>
-      <Pressable
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 10,
-            backgroundColor: "white",
-            borderRadius: 50,
-            height: 45,
-            borderWidth: 1,
-            borderColor:'gray'
-          }}
-        >
-          <AntDesign
-            style={{ marginLeft: 10 }}
-            name="search1"
-            size={20}
-            color="black"
-          />
-          <TextInput placeholder="Search" />
-        </Pressable>
-      <FlatList
-        data={friends}
-        renderItem={({item}) => (
-          <MessageItem item={item}/>
-        )}
-        keyExtractor={(item, index) => item._id}
-      />
-    </View>
-    // <View>
-    //      <ZegoUIKitPrebuiltCall
-    //             appID={1937296802}
-    //             appSign={'5c4f3eeba1413c68105d3da1d407f540ad7727bcb5bc01de4fb2df4a49f789ee'}
-    //             userID={'121312'} // userID can be something like a phone number or the user id on your own user system. 
-    //             userName={'quan2121'}
-    //             callID={'group-room123'} // callID can be any unique string. 
-
-    //             config={{
-    //                 // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
-    //                 ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
-    //                 onOnlySelfInRoom: () => { props.navigation.navigate('HomePage') },
-    //                 onHangUp: () => { props.navigation.navigate('HomePage') },
-    //             }}
-    //         />
+    // <View style={styles.container}>
+    //   <Pressable
+    //       style={{
+    //         flexDirection: "row",
+    //         alignItems: "center",
+    //         gap: 10,
+    //         backgroundColor: "white",
+    //         borderRadius: 50,
+    //         height: 45,
+    //         borderWidth: 1,
+    //         borderColor:'gray'
+    //       }}
+    //     >
+    //       <AntDesign
+    //         style={{ marginLeft: 10 }}
+    //         name="search1"
+    //         size={20}
+    //         color="black"
+    //       />
+    //       <TextInput placeholder="Search" />
+    //     </Pressable>
+    //   <FlatList
+    //     data={friends}
+    //     renderItem={({item}) => (
+    //       <MessageItem item={item}/>
+    //     )}
+    //     keyExtractor={(item, index) => item._id}
+    //   />
     // </View>
+    <View>
+         <ZegoUIKitPrebuiltCall
+                appID={1937296802}
+                appSign={'5c4f3eeba1413c68105d3da1d407f540ad7727bcb5bc01de4fb2df4a49f789ee'}
+                userID={'121312'} // userID can be something like a phone number or the user id on your own user system. 
+                userName={'quan2121'}
+                callID={'group-room123'} // callID can be any unique string. 
+
+                config={{
+                    // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
+                    ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
+                    onOnlySelfInRoom: () => { props.navigation.navigate('HomePage') },
+                    onHangUp: () => { props.navigation.navigate('HomePage') },
+                }}
+            />
+    </View>
   );
 };
 
